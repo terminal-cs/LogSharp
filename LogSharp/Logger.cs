dede39ba6c459edb6a6a6e600e99a3e2be0826f9
Evan Lawrence <evanlawrence@cyberfear.com>
2023-01-11 01:28:58 -0800

    Add file logging option.

--- LogSharp/Logger.cs
+++ LogSharp/Logger.cs
@@ -9,9 +9,24 @@
 		/// Creates a new instance of the <see cref="Logger"/> class.
 		/// </summary>
 		/// <param name="Category">Category to print as (e.g. Kernel, Graphics... etc)</param>
+		/// <param name="Kind">The kind of logger, see the <see cref="LoggerKind"/> enum for more details.</param>
+		/// <param name="StorePath">The path to store logging data, if storing is enabled.</param>
+		public Logger(string Category, LoggerKind Kind = LoggerKind.JustPrint, string StorePath = "LogSharp.txt")
+		{
+			this.Category = Category;
+			this.Kind = Kind;
+			this.StorePath = StorePath;
+		}
+
+		/// <summary>
+		/// Creates a new instance of the <see cref="Logger"/> class.
+		/// </summary>
+		/// <param name="Category">Category to print as (e.g. Kernel, Graphics... etc)</param>
 		public Logger(string Category)
 		{
 			this.Category = Category;
+			Kind = LoggerKind.JustPrint;
+			StorePath = string.Empty;
 		}
 
 		#region Methods
@@ -23,21 +38,28 @@
 		/// <param name="S">Sevetiry of the message.</param>
 		public void Log(string Message, Severity S = Severity.Info)
 		{
-			switch (S)
+			if (Kind == LoggerKind.JustPrint || Kind == LoggerKind.Both)
 			{
-				case Severity.Warning:
-					Console.ForegroundColor = ConsoleColor.Yellow;
-					break;
-				case Severity.Critical:
-					Console.ForegroundColor = ConsoleColor.Red;
-					break;
-				case Severity.Ok:
-					Console.ForegroundColor = ConsoleColor.Green;
-					break;
+				switch (S)
+				{
+					case Severity.Warning:
+						Console.ForegroundColor = ConsoleColor.Yellow;
+						break;
+					case Severity.Critical:
+						Console.ForegroundColor = ConsoleColor.Red;
+						break;
+					case Severity.Ok:
+						Console.ForegroundColor = ConsoleColor.Green;
+						break;
+				}
+				Console.Write($"[ {S} ] ");
+				Console.ResetColor();
+				Console.WriteLine($"{Category} > {Message}");
 			}
-			Console.Write($"[ {S} ] ");
-			Console.ResetColor();
-			Console.WriteLine($"{Category} > {Message}");
+			if (Kind == LoggerKind.JustStore || Kind == LoggerKind.Both)
+			{
+				File.AppendAllText(StorePath, $"[ {S} ] {Category} > {Message}");
+			}
 		}
 
 		/// <summary>
@@ -84,6 +106,9 @@
 		/// Category marker for the debugger instance.
 		/// </summary>
 		public string Category;
+
+		private string StorePath;
+		private LoggerKind Kind;
 
 		#endregion
 	}
---
+++ LogSharp/LoggerKind.cs
@@ -0,0 +1,12 @@
+﻿namespace LogSharp
+{
+	/// <summary>
+	/// The kind of logger that is defined.
+	/// </summary>
+	public enum LoggerKind
+	{
+		JustStore,  // Just store output to the specified file.
+		JustPrint,  // Just print output to the console.
+		Both,		// Just do both.
+	}
+}
