42bec98300c5312d26d6ed851a9a07b574e6303d
Evan Lawrence <evanlawrence@cyberfear.com>
2023-01-11 01:38:42 -0800

    Update comments.

--- LogSharp/LoggerKind.cs
+++ LogSharp/LoggerKind.cs
@@ -2,11 +2,21 @@
 {
 	/// <summary>
 	/// The kind of logger that is defined.
+	/// Note: Logging to a file could cause major slowdowns if the host system's disks aren't fast.
 	/// </summary>
 	public enum LoggerKind
 	{
-		JustStore,  // Just store output to the specified file.
-		JustPrint,  // Just print output to the console.
-		Both,		// Just do both.
+		/// <summary>
+		/// Just store output to the specified file.
+		/// </summary>
+		JustStore,
+		/// <summary>
+		/// Just print output to the console.
+		/// </summary>
+		JustPrint,
+		/// <summary>
+		/// Just do both.
+		/// </summary>
+		Both,
 	}
 }
